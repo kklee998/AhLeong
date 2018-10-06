891b5638d2ee739881153aa1e2cdf29e46e2d5cd
kklee998 <kahkinlee998@gmail.com>
2018-10-06 20:40:31 +0800

    fixed login

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const
   bodyParser = require('body-parser'),
   app = express().use(bodyParser.json()), // creates express http server
   PAGE_ACCESS_TOKEN = 'EAAP34enEWtgBAOpqWmZCmHNg5jhA5L1loWbcUZBPjB1WFTN98fM9KZBUq3gYR9oCENCIgLKKgSdDLxn2qwyGfmtWUrF6xcsjG0RuNJjZAzRslZAGajRinemCegK18apJXBeBRxz2BRlneF0asN9RdVm6AZAtZCL1Eh0ZBDRgO3M8xgZDZD';
-
+  app.use(express.static('public'));
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
@@ -25,7 +25,7 @@ app.get('/login', (req, res, next) => {
 });
 
 // Handle postback from webview
-app.get('/loginpostback', (req, res) => {
+app.post('/loginpostback', (req, res) => {
     let body = req.query;
     let response;
     if( body.username === 'admin' && body.password === 'admin123' ){
@@ -42,9 +42,9 @@ app.get('/loginpostback', (req, res) => {
     callSendAPI(body.psid, response);
 });
 
-// Creates the endpoint for our webhook 
-app.post('/webhook', (req, res) => {  
- 
+// Creates the endpoint for our webhook
+app.post('/webhook', (req, res) => {
+
   let body = req.body;
 
   // Checks this is an event from a page subscription
@@ -85,25 +85,25 @@ app.get('/webhook', (req, res) => {
 
   // Your verify token. Should be a random string.
   let VERIFY_TOKEN = "EAAP34enEWtgBANzSTJMHL6mbhNAL7eyO4q1jpzjOX3E0wEmue6QTGt8u5Wjtv53a7pX3Xx92cqkxQXG5qQvnsnYrqsmnhyZCr2o6rxT8ED83SeR1LwyjZCCzYA2StZC7Qzos8hJvouT0WyYAG8EZAdeoji48GxPFVPrxqWkaIAZDZD"
-    
+
   // Parse the query params
   let mode = req.query['hub.mode'];
   let token = req.query['hub.verify_token'];
   let challenge = req.query['hub.challenge'];
-    
+
   // Checks if a token and mode is in the query string of the request
   if (mode && token) {
-  
+
     // Checks the mode and token sent is correct
     if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      
+
       // Responds with the challenge token from the request
       console.log('WEBHOOK_VERIFIED');
       res.status(200).send(challenge);
-    
+
     } else {
       // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);      
+      res.sendStatus(403);
     }
   }
 });
@@ -176,17 +176,5 @@ function login(sender_psid) {
     }
   };
 
-  // Send the HTTP request to the Messenger Platform
-  request({
-    "uri": "https://graph.facebook.com/v2.6/me/messages",
-    "qs": { "access_token": PAGE_ACCESS_TOKEN },
-    "method": "POST",
-    "json": request_body
-  }, (err, res, body) => {
-    if (!err) {
-      console.log('message sent!')
-    } else {
-      console.error("Unable to send message:" + err);
-    }
-  });
+  return request_body;
 }
