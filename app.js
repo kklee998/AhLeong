169eae1fdd423e6723ab5237d92870b5e499bc49
MSI\Kelvin <kelvinlim_98@live.com>
2018-10-07 03:36:14 +0800

    Modified Chain Logging

--- app.js
+++ app.js
@@ -433,8 +433,9 @@ function login(sender_psid) {
 }
 
 function readChainNo(sender_psid){
-  var jsonData = require('./' + sender_psid + '-chain.json');
+  var jsonData = require(sender_psid + '-chain.json');
   var test = JSON.stringify(jsonData)
+  console.log(test);
   console.log(test.chain);
   if(test.chain){
     return test.chain;
