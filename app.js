fd285ee7464a0f9bdd50c14d5ead0e4aa3d25fda
MSI\Kelvin <kelvinlim_98@live.com>
2018-10-06 20:35:57 +0800

    Bug Fixes

--- app.js
+++ app.js
@@ -175,18 +175,19 @@ function login(sender_psid) {
         }
     }
   };
-  console.log(request_body);
+  
+  return request_body;
   // Send the HTTP request to the Messenger Platform
-  request({
-    "uri": "https://graph.facebook.com/v2.6/me/messages",
-    "qs": { "access_token": PAGE_ACCESS_TOKEN },
-    "method": "POST",
-    "json": request_body
-  }, (err, res, body) => {
-    if (!err) {
-      console.log('message sent!')
-    } else {
-      console.error("Unable to send message:" + err);
-    }
-  });
+  // request({
+  //   "uri": "https://graph.facebook.com/v2.6/me/messages",
+  //   "qs": { "access_token": PAGE_ACCESS_TOKEN },
+  //   "method": "POST",
+  //   "json": request_body
+  // }, (err, res, body) => {
+  //   if (!err) {
+  //     console.log('message sent!')
+  //   } else {
+  //     console.error("Unable to send message:" + err);
+  //   }
+  // });
 }
