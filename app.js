3acdf908702ace95cbb7a87c2f8620d7da3befc2
MSI\Kelvin <kelvinlim_98@live.com>
2018-10-06 21:54:56 +0800

    Testing

--- app.js
+++ app.js
@@ -171,7 +171,7 @@ function login(sender_psid) {
             text: "OK, let's log in to your Hong Leong Bank account first.",
             buttons: [{
                 type: "web_url",
-                url: "ahleong-kelvin.herokuapp.com/login",
+                url: "https://ahleong-kelvin.herokuapp.com/login",
                 title: "Login",
                 webview_height_ratio: "compact",
                 messenger_extensions: false
