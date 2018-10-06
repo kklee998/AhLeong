37d8329524290c36854c7b570fb6615568d8f5e9
kklee998 <kahkinlee998@gmail.com>
2018-10-06 21:43:25 +0800

    fixed typo

--- index.js
+++ index.js
@@ -56,11 +56,11 @@ app.post('/webhook', (req, res) => {
 
     // Gets the body of the webhook event
     let webhook_event = entry.messaging[0];
-    console.log(webhook_event);
+    //console.log(webhook_event);
 
     // Get the sender PSID
     let sender_psid = webhook_event.sender.id;
-    console.log('Sender PSID: ' + sender_psid);
+    //console.log('Sender PSID: ' + sender_psid);
 
     let lastMsgTimestamp = webhook_event.timestamp;
 
